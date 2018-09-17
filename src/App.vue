314f55045903465ebea276a039a7c54a0dfe12f9
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-09-17 19:49:40 +0200

    Layout-NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+     <nav-bar></nav-bar>
   </div>
 </template>
+
+<script>
+
+import NavBar from './components/NavBar'
+
+export default {
+    name:'App',
+    components:{
+        NavBar
+    }
+}
+
+</script>
+<style>
 
 <style>
 #app {
@@ -16,16 +26,5 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"><i class="far fa-image"></i> Gallery</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">All Galleries</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My Galleries</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-pencil-alt"></i> Create New Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="far fa-user"></i> Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                </li>
+     
+            </ul>
+        </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'NavBar',
+    
+}
+
+</script>
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
