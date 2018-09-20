f02c3374c68b8918fca09696b0d9369dc65af471
Natasa <moj_email@gmail.com>
2018-09-20 13:01:21 +0200

    authors page paginate

--- src/containers/AuthorsGalleries.vue
+++ src/containers/AuthorsGalleries.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app"></br>
-    <div class="polaroid" v-for="gallery in galleries" :key="gallery.id">
+    <div class="polaroid" v-for="gallery in loadMoreGalleries" :key="gallery.id">
         <img :src="gallery.images[0].url " alt="Image" style="width:100%">
         <div class="container">
             <router-link :to="{name: 'single-gallery', params: {id: gallery.id}}">
             {{ gallery.name }}</router-link>
          </div>
     </div>
+    <div>
+        <button  v-if="galleries.next_page_url" @click="loadMore" class="btn btn-dark btn-sm">Load more...</button>
+      </div>
   </div>
 </template>
 
@@ -17,7 +20,8 @@ import { galleries } from '../services/Gallery'
 export default {
   data(){
       return{
-          galleries:[]
+          galleries:[],
+          loadMoreGalleries:[]
       }
   },
 
@@ -25,12 +29,30 @@ export default {
         galleries.getAuthorGalleries(this.$route.params.id)
         .then(response => {
             this.galleries = response.data
+            this.paginateAuthorsGalleries(this.galleries)
         })
         .catch(error => {
                 this.error = error.response.data.error
         })
+    },
+
+    methods:{
+        paginateAuthorsGalleries(galleries) {
+            this.galleries = galleries
+            this.loadMoreGalleries = this.galleries.data
+        },
+
+       loadMore() {
+            galleries.getNextPage(this.galleries.next_page_url)
+              .then((loadedGalleries) => {
+                    this.galleries = loadedGalleries.data
+                    for(var i = 0; i < loadedGalleries.data.data.length; i++) {
+                        this.loadMoreGalleries.push(loadedGalleries.data.data[i])
+                    }
+                })
+             },
+        }
     }
-}
 
 </script>
 
